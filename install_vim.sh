dd658dc7abbbbe7207bdcc158644c5bd66739d8e
Kenneth Bruce <k.bruce16@gmail.com>
2018-02-27 10:34:10 -0500

    added airline install for vim

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo apt install vim curl
+
+#install pathogen, then install pathogen plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
