c67034c08d4e0901508fecd8e5bc7f5e0cc983de
root <root@somnus.aceofpengwins.com>
2018-09-22 19:08:47 -0400

    test

--- install_vim.sh
+++ install_vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y vim curl
+#sudo yum install -y vim curl
 
 #install pathogen, then install pathogen plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
