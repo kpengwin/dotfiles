7af7fbde7aee51a68c6b51564f9a9fa0f418071b
Kenny <me@kenneth-bruce.com>
2022-12-22 08:59:54 -0500

    add plugins

--- install_vim.sh
+++ install_vim.sh
@@ -7,7 +7,9 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
+git clone https://github.com/sheerun/vim-polyglot.git ~/.vim/bundle/vim-polyglot
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 
 #this is maybe complicated
 #git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
